b847fe2340b9be02ed6045c44b38ad5849377a07
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-02-25 20:14:16 +0700

    Update I2C_Slave.ino

--- Src/I2C_Slave.ino
+++ Src/I2C_Slave.ino
@@ -6,7 +6,7 @@
 #define slaveAddress 1 // defining I2C address to connect with Master
 #define dataretrievalSize 23 // defining size on data retrieval
 #define dataTransmissionSize 27 // defining size on data transmission
-String response; // string variable used to send data to Arduino Mega
+String sendData; // string variable used to send data to Arduino Mega
 String getData; // string variable used to get data from Arduino Mega
 
 // Method: setup
@@ -27,16 +27,16 @@ void loop() {
 void sensor(){
   int sensorState = digitalRead(raindropSensorPin); // read the sensor on digital: D2
   if(sensorState == LOW){ // sensor detected wet then
-    response = "Rain Warning"; // print to serial monitor
+    sendData = "Rain Warning"; // print to serial monitor
   }
   if(sensorState == HIGH){ // sensor detected dry then
-    response = "Not Raining"; // print to serial monitor
+    sendData = "Not Raining"; // print to serial monitor
   }
 }
 
 // Method: dataRetrieval
 void dataRetrieval(){
-  Wire.requestFrom(slaveAddress,dataretrievalSize); // read response from Master
+  Wire.requestFrom(slaveAddress,dataretrievalSize); // read data from Master
   getData = ""; while(Wire.available()){ getData += char(Wire.read()); } // add characters to string
   Serial.print("Data retrieval\t\t: "); Serial.println(getData); // print to serial monitor
 }
@@ -44,7 +44,7 @@ void dataRetrieval(){
 // Method: dataTransmission
 void dataTransmission(){
   byte data[dataTransmissionSize]; // setup byte variable in the correct size
-  for(byte i=0;i<dataTransmissionSize;i++) { data[i] = (byte)response.charAt(i); } // format data as array
-  Wire.write(data,sizeof(data)); // send response to Master
-  Serial.print("Data transmission\t: "); Serial.println(response); // print to serial monitor
+  for(byte i=0;i<dataTransmissionSize;i++) { data[i] = (byte)sendData.charAt(i); } // format sendData as array
+  Wire.write(data,sizeof(data)); // send data to Master
+  Serial.print("Data transmission\t: "); Serial.println(sendData); // print to serial monitor
 }
